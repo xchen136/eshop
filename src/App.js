565b5f238a30e501c68924761ad7a4d284205837
Software Engineer <marpipe@marpipe.myfiosgateway.com>
2023-05-19 20:05:30 -0400

    Update 'App' component and its stylesheet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Home from './Home'
+import Header from './Header'
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      <Home />
     </div>
   );
 }
