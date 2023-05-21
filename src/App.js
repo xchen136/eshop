2d239e4baf21e7a4731e98a78af7707768897ab5
Software Engineer <marpipe@marpipe.myfiosgateway.com>
2023-05-21 13:28:19 -0400

    Create 'Checkout' component and implement routing to it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import Home from './Home'
 import Header from './Header'
+import Checkout from './Checkout'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Home />
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/checkout" element={<Checkout/>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -4,16 +4,18 @@ import './Header.css';
 import StorefrontIcon from '@mui/icons-material/Storefront';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import SearchIcon from '@mui/icons-material/Search';
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
+  const navigate = useNavigate()
   return (
     <div className="header">
-      <div className="header__logo">
+      <div className="header__logo" onClick={() => navigate('/')}>
         <StorefrontIcon className="header__logoImage" fontSize="large"/>
         <h2 className="header__logoTitle">eShop</h2>
       </div>
 
-      <div className="header__search">
+      <div className="header__search"> 
         <input type="text" className="header__searchInput"/>
         <SearchIcon className="header__searchIcon"/>
       </div>
@@ -23,11 +25,13 @@ function Header() {
           <span className="nav__itemLineOne">Hello Guest</span>
           <span className="nav__itemLineTwo">Sign In</span>
         </div>
+
         <div className="nav__item">
           <span className="nav__itemLineOne">Your</span>
           <span className="nav__itemLineTwo">Shop</span>
         </div>
-        <div className="nav__itemBasket">
+
+        <div className="nav__itemBasket" onClick={() => navigate('/checkout')}>
           <ShoppingBasketIcon/>
           <span className="nav__itemLineTw nav__basketCount">0</span>
         </div>
