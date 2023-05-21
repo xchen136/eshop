3c821a94945fd066c1ada815a3dd435509290c6a
Software Engineer <marpipe@marpipe.myfiosgateway.com>
2023-05-21 13:29:29 -0400

    Create 'CheckoutProduct' component and its stylesheet

---
+++ src/CheckoutProduct.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './CheckoutProduct.css'
+
+function CheckoutProduct() {
+  return (
+    <div className="checkoutProduct">
+      <img src="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg" alt="" className="checkoutProduct__img" />
+
+      <div className="checkoutProduct__info">
+        <p className="checkoutProduct__title">
+          Bennett Mystic 15.6 inch Laptop Shoulder Messenger Sling Office Bag, Water Repellent Fabric for Men and Women (Blue)
+        </p>
+
+        <p className="checkoutProduct__price">
+            <small>$</small>
+            <strong>15.99</strong>
+        </p>
+
+        <div className="checkoutProduct__rating">
+          { Array(5).fill().map((_,i) => (<p>⭐️</p>))}
+        </div>
+
+        <button className="checkoutProduct__btn">Remove from Basket</button>
+      </div>
+    </div>
+
+    
+  )
+}
+
+export default CheckoutProduct
